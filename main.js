db1792f13a1214b86164b5c11a004b40f784f5b9
bansalruhani <76994163+bansalruhani@users.noreply.github.com>
2021-08-12 15:36:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+prediction_1 = ""
+ prediction_2 = ""
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="capture_image"src"'+data_uri+'"/>';
+    });
+}
+console.log('ml5 version:',ml5.version);
+classifer=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/2gTek_13t/model.json',modeLoaded);
+function modeLoaded(){
+    console.log('Model Loaded!');
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data_1="the first prediction is"+prediction_1;
+    speak_data_2="And the second prediction is"+prediction_2;
+    var utterThis=new SpeechSynthesisUtterance(speach_data_1+speak_data_2);
+    synth.speak(utterThis);
+}
